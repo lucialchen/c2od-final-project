3c08b89bd7197c5ada7458414895da09b45ccc58
Lucia Chen <luciachen@Lucias-MacBook-Air-4.local>
2025-11-20 20:51:27 -0500

    Implemented PopulationReader + TotalPopulation.

--- src/main/java/municipaldata/data/JSONReader.java
+++ src/main/java/municipaldata/data/JSONReader.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import municipaldata.common.ParkingViolation;
 
 public class JSONReader implements ParkViolationFileType {
