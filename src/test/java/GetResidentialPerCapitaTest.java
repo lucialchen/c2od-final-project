f72fb80dcdc1f98ae8535590a7ec795b5c472272
Lucia Chen <luciachen@Lucias-Air-6.lan>
2025-12-09 12:22:27 -0500

    edits.

--- src/test/java/GetResidentialPerCapitaTest.java
+++ src/test/java/GetResidentialPerCapitaTest.java
@@ -39,7 +39,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 100);
+                map.put("89999", 100);
                 return map;
             }
         };
@@ -48,8 +48,8 @@ public class GetResidentialPerCapitaTest {
             public Map<String, ArrayList<PropertyValue>> getPropertyValues() {
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(200.0, 150.0, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(200.0, 150.0, "89999"));
+                map.put("89999", values);
                 return map;
             }
         };
@@ -64,7 +64,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 100);
+                map.put("88899", 100);
                 return map; 
             }
         };
@@ -73,13 +73,13 @@ public class GetResidentialPerCapitaTest {
             public Map<String, ArrayList<PropertyValue>> getPropertyValues() {
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(200.0, 150.0, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(200.0, 150.0, "88899"));
+                map.put("88899", values);
                 return map;
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "9899");
+        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "8889");
         assertEquals(0, result);
     }
 
@@ -89,7 +89,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 100);
+                map.put("10101", 100);
                 return map;
             }
         };
@@ -99,13 +99,13 @@ public class GetResidentialPerCapitaTest {
                 System.out.println("getPropertyValues() called!"); 
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(200.0, 150.0, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(200.0, 150.0, "10101"));
+                map.put("10101", values);
                 return map;
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "989999099");
+        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "10101011111");
         assertEquals(2, result);  
     }
 
@@ -115,7 +115,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 100);
+                map.put("29292", 100);
                 return map;
             }
         };
@@ -124,14 +124,14 @@ public class GetResidentialPerCapitaTest {
             public Map<String, ArrayList<PropertyValue>> getPropertyValues() {
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(200.0, 150.0, "98999"));
-                values.add(new PropertyValue(300.0, 250.0, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(200.0, 150.0, "29292"));
+                values.add(new PropertyValue(300.0, 250.0, "29292"));
+                map.put("29292", values);
                 return map;
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "98999");
+        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "29292");
         assertEquals(5, result);    
     }
 
@@ -149,13 +149,13 @@ public class GetResidentialPerCapitaTest {
             public Map<String, ArrayList<PropertyValue>> getPropertyValues() {
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(200.0, 150.0, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(200.0, 150.0, "30303"));
+                map.put("30303", values);
                 return map;
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "98999");
+        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "30303");
         assertEquals(0, result);    
     }
 
@@ -165,7 +165,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 0);
+                map.put("40404", 0);
                 return map;
             }
         };
@@ -174,13 +174,13 @@ public class GetResidentialPerCapitaTest {
             public Map<String, ArrayList<PropertyValue>> getPropertyValues() {
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(200.0, 150.0, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(200.0, 150.0, "40404"));
+                map.put("40404", values);
                 return map;
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "98999");
+        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "40404");
         assertEquals(0, result);    
     }
 
@@ -190,7 +190,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 100);
+                map.put("50505", 100);
                 return map;
             }
         };
@@ -202,7 +202,7 @@ public class GetResidentialPerCapitaTest {
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "98999");
+        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "50505");
         assertEquals(0, result);    
     }
 
@@ -212,7 +212,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 50);
+                map.put("59595", 50);
                 return map;
             }
         };
@@ -221,14 +221,14 @@ public class GetResidentialPerCapitaTest {
             public Map<String, ArrayList<PropertyValue>> getPropertyValues() {
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(200.0, 150.0, "98999"));
-                values.add(new PropertyValue(300.0, 250.0, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(200.0, 150.0, "59595"));
+                values.add(new PropertyValue(300.0, 250.0, "59595"));
+                map.put("59595", values);
                 return map;
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "98999");
+        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "59595");
         assertEquals(10, result);    
     }
 
@@ -238,7 +238,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 50);
+                map.put("12121", 50);
                 return map;
             }
         };
@@ -247,15 +247,15 @@ public class GetResidentialPerCapitaTest {
             public Map<String, ArrayList<PropertyValue>> getPropertyValues() {
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(200.0, 150.0, "98999"));
-                values.add(new PropertyValue(300.0, 250.0, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(200.0, 150.0, "12121"));
+                values.add(new PropertyValue(300.0, 250.0, "12121"));
+                map.put("12121", values);
                 return map;
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result1 = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "98999");
-        int result2 = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "98999");
+        int result1 = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "12121");
+        int result2 = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "12121");
         assertEquals(10, result1);
         assertEquals(10, result2);
     }
@@ -266,7 +266,7 @@ public class GetResidentialPerCapitaTest {
             @Override
             public Map<String, Integer> getPopulationData() {
                 Map<String, Integer> map = new TreeMap<>();
-                map.put("98999", 100);
+                map.put("79797", 100);
                 return map;
             }
         };
@@ -275,13 +275,13 @@ public class GetResidentialPerCapitaTest {
             public Map<String, ArrayList<PropertyValue>> getPropertyValues() {
                 Map<String, ArrayList<PropertyValue>> map = new TreeMap<>();
                 ArrayList<PropertyValue> values = new ArrayList<>();
-                values.add(new PropertyValue(null, null, "98999"));
-                map.put("98999", values);
+                values.add(new PropertyValue(null, null, "79797"));
+                map.put("79797", values);
                 return map;
             }
         };
         ResidentialPerCapita rpc = new ResidentialPerCapita(pr, pvr);
-        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "98999");
+        int result = rpc.getPerCapita(ResidentialMode.MARKET_VALUE, "79797");
         assertEquals(0, result);
     }
 
