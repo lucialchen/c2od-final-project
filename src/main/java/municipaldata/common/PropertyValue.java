7f0a44ad423e150c26372ad99adc9677a1dc8f0e
Lucia Chen <luciachen@Lucias-Air-6.lan>
2025-12-08 21:19:41 -0500

    Refractored ProperyValue to use Double wrappers.

--- src/main/java/municipaldata/common/PropertyValue.java
+++ src/main/java/municipaldata/common/PropertyValue.java
@@ -6,7 +6,7 @@ public class PropertyValue {
     double total_livable_area;
     String zip_code;
 
-    public PropertyValue(double market_value, double total_livable_area, String zip_code) {
+    public PropertyValue(Double market_value, Double total_livable_area, String zip_code) {
         this.market_value = market_value;
         this.total_livable_area = total_livable_area;
         this.zip_code = zip_code;
@@ -16,7 +16,7 @@ public class PropertyValue {
         return zip_code;
     }
 
-    public double getData(ResidentialMode m) {
+    public Double getData(ResidentialMode m) {
         if (m == ResidentialMode.MARKET_VALUE) {
             return market_value;
         } else {
